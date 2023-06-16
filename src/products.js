59a82841da736bd3d740b09aa53aaa6557fb09a1
yashkumawat21 <kumawatyash2190@gmail.com>
2023-06-17 01:19:06 +0530

    first commit

---
+++ src/products.js
@@ -0,0 +1,31 @@
+import img1 from "./assets/1.png"
+import img2 from "./assets/2.png"
+import img3 from "./assets/3.png"
+
+export const PRODUCTS = [
+
+    {
+
+        id :1 ,
+        productName : "IPhone",
+        price :99.0,
+        productImage : img1,
+    }
+    ,
+    {
+
+        id :2 ,
+        productName : "Binggg",
+        price :989.0,
+        productImage : img2,
+    }
+    ,
+    {
+
+        id :3 ,
+        productName : "hofdfne",
+        price :9965.0,
+        productImage : img3
+    }
+    
+]
